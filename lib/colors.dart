18f7d7c3dd75b870fc20aae069d13bb83bb98221
Sebastian Schwarzer <scse1047@hs-karlsruhe.de>
2019-11-20 15:43:05 +0100

    hide color names

--- lib/colors.dart
+++ lib/colors.dart
@@ -2,17 +2,17 @@
 import 'package:flutter/material.dart';
 
 // Colordefinitions
-const tealBlue = const Color(0xFF024959);
-const blueLagoon = const Color(0xFF037E8C);
-const blackWhite = const Color(0xFFFFFFFA);
-const pomegranate = const Color(0xFFF24C27);
-const soyaBean = const Color(0xFF5F5448);
+const _tealBlue = const Color(0xFF024959);
+const _blueLagoon = const Color(0xFF037E8C);
+const _blackWhite = const Color(0xFFFFFFFA);
+const _pomegranate = const Color(0xFFF24C27);
+const _soyaBean = const Color(0xFF5F5448);
 
 // Specify the use and reuse of the variables above
-const primaryColor = tealBlue;
-const cardColor = blackWhite;
-const backgroundColor = blueLagoon;
-const warningColor = pomegranate;
+const primaryColor = _tealBlue;
+const cardColor = _blackWhite;
+const backgroundColor = _blueLagoon;
+const warningColor = _pomegranate;
 
 // Declare themes
 // TODO: Remove temporary theme any apply above colors
